2094f31bb2e632c10d10aeaac70032f9b8a3ad8c
seraaboagye <sera.aboagye@gmail.com>
2020-01-28 15:11:19 -0600

    variables

--- Launch_EC2_instance/vars.tf
+++ Launch_EC2_instance/vars.tf
@@ -1,7 +1,9 @@
 variable "AWS_ACCESS_KEY" {
+  default = AKIARM5GCCINSXYS2HWI
 }
 
 variable "AWS_SECRET_KEY" {
+  default = PS1WREBvYBmDZI0JXD4MG9KLnCeqdJ1AqmM/f6br
 }
 
 variable "AWS_REGION" {
@@ -11,8 +13,8 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-086be1694c3196bfb"
+    us-west-2 = "ami-06c94f9acb4ba21b2"
+    eu-west-1 = "ami-0b508e9b54730f606"
   }
 }
